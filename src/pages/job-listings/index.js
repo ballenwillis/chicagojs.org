b70f1fad81847cef21673860fa31ebd4e9661b2d
Brandon Willis <bwmusiclover1212@gmail.com>
2018-11-19 23:03:30 -0600

    Flex directions are working

--- src/pages/job-listings/index.js
+++ src/pages/job-listings/index.js
@@ -3,16 +3,15 @@ import { graphql } from 'gatsby'
 import get from 'lodash.get'
 import Layout from '../../components/Layout'
 
-const container = {
+const jobListingContainer = {
   borderWidth: '5px',
   borderColor: 'red',
-  height: '80px',
-  backgroundColor: 'green'
+  height: '80px'
 }
 
 const JobListing = ({ neighborhood, position, datePosted, positionType, jobLink }) => (
-  <div className="col-sm-12" style={container}>
-    <div className="row">
+  <div className="col-sm-12" style={jobListingContainer}>
+    <div className="row" style={{ justifyContent: 'space-between' }}>
       <div>
         <a href={jobLink}>
           <h6>{position}</h6>
