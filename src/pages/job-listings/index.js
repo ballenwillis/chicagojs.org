d747e525f295fe4c5240ffa5d4b4b3e12facabc8
Brandon Willis <bwmusiclover1212@gmail.com>
2018-11-19 22:50:20 -0600

    Starting to render things...

--- src/pages/job-listings/index.js
+++ src/pages/job-listings/index.js
@@ -1,13 +1,47 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import get from 'lodash.get'
+import Layout from '../../components/Layout'
+
+const container = {
+  borderWidth: '5px',
+  borderColor: 'red',
+  height: '80px',
+  backgroundColor: 'green'
+}
+
+const JobListing = ({ neighborhood, position, datePosted, positionType, jobLink }) => (
+  <div className="col-sm-12" style={container}>
+    <div className="row">
+      <div align="left">
+        <a href={jobLink}>
+          <h6>{position}</h6>
+        </a>
+      </div>
+      <div align="right">
+        <h6>{neighborhood}</h6>
+      </div>
+    </div>
+    <div className="row">
+      <h6>{positionType}</h6>
+      <h6>{datePosted}</h6>
+    </div>
+  </div>
+)
 
 export default class JobListingsPage extends React.Component {
   render() {
+    const jobs = get(this.props, 'data.allJobListingsJson.edges', [])
     return (
-      <div>
-        <h1>Company:</h1>
-        <pre>{JSON.stringify(this.props.data)}</pre>
-      </div>
+      <Layout>
+        <div className="container">
+          <div className="row">
+            {jobs.map(j => (
+              <JobListing {...j.node} />
+            ))}
+          </div>
+        </div>
+      </Layout>
     )
   }
 }
@@ -17,11 +51,11 @@ export const query = graphql`
     allJobListingsJson {
       edges {
         node {
-          id
           neighborhood
           position
           datePosted
           positionType
+          jobLink
         }
       }
     }
